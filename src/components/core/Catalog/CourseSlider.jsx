6805410f6ebdf3c4daf774d8ef50f3c727c70022
parbhatsingh5211 <parbhatsingh.sr@gmail.com>
2024-06-19 02:07:49 +0530

    modified files

--- src/components/core/Catalog/CourseSlider.jsx
+++ src/components/core/Catalog/CourseSlider.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
 
-const CourseSlider = () => {
+import { Swiper, SwiperSlide } from "swiper/react"
+import "swiper/css"
+import "swiper/css/free-mode"
+import "swiper/css/pagination"
+import "swiper/css/navigation"
+import { Autoplay, Pagination } from 'swiper/modules';
+
+import Course_Card from './Course_Card'
+
+const CourseSlider = ({Courses}) => {
+
   return (
-    <div>CourseSlider</div>
+    <>
+      {Courses?.length ? (
+        <Swiper
+          slidesPerView={1}
+          loop={true}
+          spaceBetween={50}
+          pagination={true}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          breakpoints={{ 1024: { slidesPerView: 3 } }}
+          modules={[Autoplay, Pagination]}
+          className='mySwiper'
+        >
+          {Courses?.map((course, index) => (
+            <SwiperSlide key={index}>
+              <Course_Card course={course}  />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      ) : (
+        <p className="text-xl text-richblack-5">No Course Found</p>
+      )}
+    </>
   )
 }
 
--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -6,9 +6,13 @@ import { categories } from '../services/apis';
 import { getCatalogPageData } from '../services/operations/pageAndComponentData'
 import CourseSlider from '../components/core/Catalog/CourseSlider';
 import Course_Card from '../components/core/Catalog/Course_Card';
+import { useSelector } from 'react-redux';
+import Error from "./Error"
 
 const Catalog = () => {
+    const { loading } = useSelector((state) => state.profile)
     const { catalogName } = useParams();
+    const [active, setActive] = useState(1)
     const [ catalogPageData, setCatalogPageData ] = useState(null);
     const [ categoryId, setCategoryId ] = useState("");
 
@@ -25,7 +29,7 @@ const Catalog = () => {
         const getCategoryDetails = async() => {
             try {
                 const res = await getCatalogPageData(categoryId);
-                console.log("PRinting res: ", res);
+                // console.log("PRinting res: ", res);
                 setCatalogPageData(res);
             } catch (error) {
                 console.log(error);
@@ -36,66 +40,99 @@ const Catalog = () => {
         }
     }, [categoryId]);
     
+    console.log("RES: ", catalogPageData)
+    
+    if (loading || !catalogPageData) {
+        return (
+          <div className="grid min-h-[calc(100vh-3.5rem)] place-items-center">
+            <div className="spinner"></div>
+          </div>
+        )
+    }
+    if (!loading && !catalogPageData.success) {
+        return <Error />
+    }
+
   return (
-    <div className='text-richblack-5'>
-        <div>
-            <p>
-                {`Home / Catalog /`}
-                <span>
-                    {catalogPageData?.data?.selectedCategory?.name}
+    <>
+        {/* Hero Section */}
+        <div className=" box-content bg-richblack-800 px-4">
+            <div className="mx-auto flex min-h-[260px] max-w-maxContentTab flex-col justify-center gap-4 lg:max-w-maxContent ">
+              <p className="text-sm text-richblack-300">
+                {`Home / Catalog / `}
+                <span className="text-yellow-25">
+                  {catalogPageData?.data?.selectedCategory?.name}
                 </span>
-            </p>
-            <p>
+              </p>
+              <p className="text-3xl text-richblack-5">
                 {catalogPageData?.data?.selectedCategory?.name}
-            </p>
-            <p>
-            {catalogPageData?.data?.selectedCategory?.description}
-            </p>
+              </p>
+              <p className="max-w-[870px] text-richblack-200">
+                {catalogPageData?.data?.selectedCategory?.description}
+              </p>
+            </div>
         </div>
         
-        <div>
-            {/* Section-1 */}
-            <div>
-                <div>
-                    Course to get you started
-                </div>
-                <div className='flex gap-x-3'>
-                    <p>Most Popular</p>
-                    <p>New</p>
-                </div>
-                <div>
-                    <CourseSlider Courses={catalogPageData?.data?.selectedCategory?.courses}/>
-                </div>
+       {/* Section 1 */}
+       <div className=" mx-auto box-content w-full max-w-maxContentTab px-4 py-12 lg:max-w-maxContent">
+            <div className="section_heading">Courses to get you started</div>
+            <div className="my-4 flex border-b border-b-richblack-600 text-sm">
+              <p
+                className={`px-4 py-2 ${
+                  active === 1
+                    ? "border-b border-b-yellow-25 text-yellow-25"
+                    : "text-richblack-50"
+                } cursor-pointer`}
+                onClick={() => setActive(1)}
+              >
+                Most Populer
+              </p>
+              <p
+                className={`px-4 py-2 ${
+                  active === 2
+                    ? "border-b border-b-yellow-25 text-yellow-25"
+                    : "text-richblack-50"
+                } cursor-pointer`}
+                onClick={() => setActive(2)}
+              >
+                New
+              </p>
             </div>
-
-            {/* section-2 */}
             <div>
-                <div>
-                    Top Course in {catalogPageData?.data?.selectedCategory?.name}
-                </div>
-                <div>
-                    <CourseSlider Courses={catalogPageData?.data?.differentCategory?.courses}/>
-                </div>
+              <CourseSlider
+                Courses={catalogPageData?.data?.selectedCategory?.courses}
+              />
             </div>
+        </div>
 
-            {/* Section-3 */}
-            <div>
-                <div>
-                    Frequently Bought
-                </div>
-                <div className='py-8'>
-                    <div className='grid grid-cols-1 lg:grid-cols-2'>
-                        {catalogPageData?.data?.mostSellingCourses?.slice(0,4)
-                        .map((course, index) => (
-                            <Course_Card course={course} key={index} height={"h-[400px]"}/>
-                        ))}
-                    </div>
-                </div>
+        {/* Section 2 */}
+        <div className=" mx-auto box-content w-full max-w-maxContentTab px-4 py-12 lg:max-w-maxContent">
+            <div className="section_heading">
+              Top courses in {catalogPageData?.data?.differentCategory?.name}
+            </div>
+            <div className="py-8">
+              <CourseSlider
+                Courses={catalogPageData?.data?.differentCategory?.courses}
+              />
+            </div>
+        </div>
+
+        {/* Section 3 */}
+        <div className=" mx-auto box-content w-full max-w-maxContentTab px-4 py-12 lg:max-w-maxContent">
+            <div className="section_heading">Frequently Bought</div>
+            <div className="py-8">
+              <div className="grid grid-cols-1 gap-6 lg:grid-cols-3">
+                {catalogPageData?.data?.mostSellingCourses
+                  ?.slice(0, 6)
+                  .map((course, i) => (
+                    <Course_Card course={course} key={i} />
+                  ))}
+              </div>
             </div>
         </div>
 
         <Footer />
-    </div>
+    </>
   )
 }
 
