7f0fc3027b5f0a1e57f0993fc10bc5775827b173
parbhatsingh5211 <parbhatsingh.sr@gmail.com>
2024-08-04 14:58:55 +0530

    update url

--- server/controllers/ResetPassword.js
+++ server/controllers/ResetPassword.js
@@ -28,7 +28,7 @@ exports.resetPasswordToken = async (req, res) => {
         console.log("DETAILS", updatedDetails);
 
         // create url
-        const url = `http://localhost:3000/update-password/${token}`;
+        const url = `https://study-notion1-dun.vercel.app//update-password/${token}`;
         
         // send mail containing the url
         await mailSender(
