600b40ace649a7b8d5b0dddd1dc32bc1f955c954
parbhatsingh5211 <parbhatsingh.sr@gmail.com>
2024-08-04 14:33:23 +0530

    modify

--- server/index.js
+++ server/index.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const app = express();
-
 const userRoutes = require('./routes/User');
 const profileRoutes = require('./routes/Profile');
 const paymentRoutes = require('./routes/Payments');
 const courseRoutes = require('./routes/Course');
 const contactUsRoute = require('./routes/Contact');
-
 const database = require('./config/database');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
@@ -14,10 +12,14 @@ const { cloudinaryConnect } = require('./config/cloudinary');
 const fileUpload = require('express-fileupload');
 const dotenv = require('dotenv');
 
-dotenv.config();
+
+// Setting up port number
 const PORT = process.env.PORT || 4000;
 
-// database Connect
+// Loading environment variables from .env file
+dotenv.config();
+
+// Connecting to database
 database.connect();
 
 // middlewares
@@ -25,7 +27,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin: ["http://localhost:3000", "https://study-notion1-dun.vercel.app/"],
+        origin: "*",
         credentials: true,
     })
 )
@@ -34,18 +36,19 @@ app.use(
         useTempFiles: true,
         tempFileDir: "/tmp"
     })
-)
+);
+
 // cloudinary connection
 cloudinaryConnect();
 
-// routes
+// Setting up routes
 app.use("/api/v1/auth", userRoutes);
 app.use("/api/v1/profile", profileRoutes);
 app.use("/api/v1/course", courseRoutes);
 app.use("/api/v1/payment", paymentRoutes);
 app.use("/api/v1/reach", contactUsRoute);
 
-// default route
+// Testing the server
 app.get("/", (req, res) => {
     return res.json({
         success: true, 
@@ -53,6 +56,7 @@ app.get("/", (req, res) => {
     });
 });
 
+// Listening to the server
 app.listen(PORT, () => {
     console.log(`App is running at ${PORT}`);
 })
